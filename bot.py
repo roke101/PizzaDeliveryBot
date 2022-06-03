d4373947d70949cc112cb9a41d83b7ec17d6eb44
roke101 <justinm1@ksu.edu>
2022-06-02 23:46:54 -0500

    first draft of green screen command

--- bot.py
+++ bot.py
@@ -1,10 +1,16 @@
 # bot.py
 import os
+import time
 import discord
 
 from discord.ext import commands
 from dotenv import load_dotenv
 from PIL import Image
+
+from pytube import YouTube
+import cv2
+import numpy as np
+import moviepy.editor as mpe
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -197,7 +203,89 @@ async def Trump(ctx):
     os.remove(os.path.join(scriptPath, lastDiscordImage))
     
     Save_Generated_Picture(ctx, trumpBase)
+    
+    
+@bot.command(name='greenscreen')
+@commands.cooldown(1, 1, type=commands.BucketType.user)
+async def Greenscreen(ctx, arg):
+    
+    lastDiscordImage = await Get_Last_Picture(ctx)
+    
+    yt = YouTube(arg)
 
+    filter = yt.streams.filter(file_extension="mp4").get_by_resolution("720p")
+    if filter.filesize < 8000000:
+        filter.download(scriptPath, filename="videoOut.mp4")
+    else:
+        await ctx.send(content='The video linked is over 8MB, please use another video.')
+        return
+    
+    video = cv2.VideoCapture(os.path.join(scriptPath, "videoOut.mp4"))
+    image = cv2.imread(os.path.join(scriptPath, lastDiscordImage))
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+    fourcc = cv2.VideoWriter_fourcc(*'XVID');
+    out = cv2.VideoWriter(os.path.join(scriptPath, 'test_vid.avi'),fourcc, video.get(cv2.CAP_PROP_FPS), (640, 480));
+
+    done = False;
+    while not done:
+
+        ret, frame = video.read()
+        if not ret:
+            done = True;
+            continue;
+        
+
+        frame = cv2.resize(frame, (640, 480))
+        image = cv2.resize(image, (640, 480))
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        
+
+        u_green = np.array([89, 255, 255])
+        l_green = np.array([36, 50, 70])
+
+        mask = cv2.inRange(frame, l_green, u_green)
+        res = cv2.bitwise_and(frame, frame, mask = mask)
+        black_pixels = np.where(
+        (frame[:, :, 0] == 0) & 
+        (frame[:, :, 1] == 0) & 
+        (frame[:, :, 2] == 0))
+        frame[black_pixels] = [1, 1, 1]
+
+        f = frame - res
+        f = np.where(f == 0, image, f)
+        f = cv2.cvtColor(f, cv2.COLOR_HSV2BGR)
+
+        #cv2.imshow("video", frame)
+        #cv2.imshow("mask", f)
+
+        out.write(f)
+        #if cv2.waitKey(25) == 27:
+        #    break
+
+    out.release()
+    video.release()
+    cv2.destroyAllWindows()
+    out = None
+
+
+    videoMPE = mpe.VideoFileClip(os.path.join(scriptPath, "videoOut.mp4"))
+    final_audio = videoMPE.audio
+
+    my_clip = mpe.VideoFileClip(os.path.join(scriptPath, "test_vid.avi"))
+
+    my_clip.audio = final_audio
+    my_clip.write_videofile(os.path.join(scriptPath, "meme.mp4"))
+    
+    await ctx.send(file=discord.File(os.path.join(scriptPath, "meme.mp4")))
+    
+    videoMPE.close()
+    my_clip.close()
+    
+    os.remove(os.path.join(scriptPath, lastDiscordImage))
+    os.remove(os.path.join(scriptPath, "videoOut.mp4"))
+    os.remove(os.path.join(scriptPath, "test_vid.avi"))
+    os.remove(os.path.join(scriptPath, "meme.mp4"))
 
 # Helper function that saves the last image posted in channel
 # and returns that images file name    
